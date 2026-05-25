66b2d186ab9cec10023110b0eb6d16e73a980ca3
John K. Harvey <johnkharvey@gmail.com>
2026-05-25 10:49:29 -0400

    Let's add another song from the goonies 2

--- src/music_goonies2song1.asm
+++ src/music_goonies2song1.asm
@@ -1,19 +1,12 @@
-MUTE_TRACK1		set	1
+MUTE_TRACK1		set	0
 MUTE_TRACK2		set	0
-MUTE_TRACK3		set	1
+MUTE_TRACK3		set	0
 MUTE_TRACK4		set	0
 
 ; Drum equates
 BASSDRUM1		equ	$5C
 HIGHSNARE		equ	$07
 SNAREDRUM		equ	$0E
-
-;BASSDRUM       equ     $0F
-BASSDRUM        equ     $5C
-DRUM4FRAMES     equ     14
-;SNAREDRUM      equ     $07
-SNAREDRUM       equ     $0E
-DRUM4FRAMES     equ     14
 
 ;=============
 Goonies2Song1Track1
@@ -30,12 +23,54 @@ Goonies2Song1Track1
 	dc.b	#<Goonies2Song1Track1
 	dc.b	#>Goonies2Song1Track1
   ELSE
+
+	; This track is odd - it's the same as track 3 but delayed one beat
+	dc.b	#$00, #12, #RESTDEFAULT ; rest
+
+Goonies2Song1Track1Restart
 	; Uses pokey_notes column P (green)
-	dc.b	#$2D, #18, #VOL06REST02 ; D
+	dc.b	#$3E, #12, #VOL04_CONST ; A
+	dc.b	#$4F, #12, #VOL04_CONST ; F
+	dc.b	#$6A, #12, #VOL04_CONST ; C
+	dc.b	#$3E, #12, #VOL04_CONST ; A
+	dc.b	#$4F, #12, #VOL04_CONST ; F
+	dc.b	#$6A, #12, #VOL04_CONST ; C
+	dc.b	#$3E, #12, #VOL04_CONST ; A
+	dc.b	#$4F, #12, #VOL04_CONST ; F
+
+	; Uses pokey_notes column P (green)
+	dc.b	#$39, #12, #VOL04_CONST ; A#
+	dc.b	#$4F, #12, #VOL04_CONST ; F
+	dc.b	#$5E, #12, #VOL04_CONST ; D
+	dc.b	#$39, #12, #VOL04_CONST ; A#
+	dc.b	#$4F, #12, #VOL04_CONST ; F
+	dc.b	#$5E, #12, #VOL04_CONST ; D
+	dc.b	#$39, #12, #VOL04_CONST ; A#
+	dc.b	#$4F, #12, #VOL04_CONST ; F
+
+	; Uses pokey_notes column P (green)
+	dc.b	#$33, #12, #VOL04_CONST ; C
+	dc.b	#$46, #12, #VOL04_CONST ; G
+	dc.b	#$53, #12, #VOL04_CONST ; E
+	dc.b	#$33, #12, #VOL04_CONST ; C
+	dc.b	#$46, #12, #VOL04_CONST ; G
+	dc.b	#$53, #12, #VOL04_CONST ; E
+	dc.b	#$33, #12, #VOL04_CONST ; C
+	dc.b	#$46, #12, #VOL04_CONST ; G
+
+	; Uses pokey_notes column P (green)
+	dc.b	#$39, #12, #VOL04_CONST ; A#
+	dc.b	#$4F, #12, #VOL04_CONST ; F
+	dc.b	#$5E, #12, #VOL04_CONST ; D
+	dc.b	#$39, #12, #VOL04_CONST ; A#
+	dc.b	#$4F, #12, #VOL04_CONST ; F
+	dc.b	#$5E, #12, #VOL04_CONST ; D
+	dc.b	#$39, #12, #VOL04_CONST ; A#
+	dc.b	#$4F, #12, #VOL04_CONST ; F
 
 	dc.b	#$FF
-	dc.b	#<Goonies2Song1Track1
-	dc.b	#>Goonies2Song1Track1
+	dc.b	#<Goonies2Song1Track1Restart
+	dc.b	#>Goonies2Song1Track1Restart
   ENDIF ; (IF MUTE_TRACK1)
 
 ;=============
@@ -56,37 +91,42 @@ Goonies2Song1Track2
 	dc.b	#>Goonies2Song1Track2
   ELSE
 	; Uses pokey_notes column H (brown)
+	dc.b	#$59, #96, #VOL06_CONST ; F 
 
-	;E?  Tiny trill on start
-        dc.b    #$59, #96, #VOL06_CONST ; F 
+	; Uses pokey_notes column H (brown)
+	dc.b	#$59, #84, #VOL06_CONST ; F - continue previous
+	dc.b	#$77, #6,  #VOL06_CONST ; C
+	dc.b	#$5E, #6,  #VOL06_CONST ; E
 
-        dc.b    #$59, #84, #VOL06_CONST ; F - continue previous
-        dc.b    #$77, #6,  #VOL06_CONST ; C
-        dc.b    #$5E, #6,  #VOL06_CONST ; E
+	; Uses pokey_notes column H (brown)
+	dc.b	#$59, #96, #VOL06_CONST ; F
 
-        dc.b    #$59, #96, #VOL06_CONST ; F
+	; Uses pokey_notes column H (brown)
+	dc.b	#$59, #24, #VOL06_CONST ; F - continue previous
+	dc.b	#$77, #12, #VOL06_CONST ; C - low
+	dc.b	#$42, #24, #VOL06_CONST ; A# - high
+	dc.b	#$3B, #24, #VOL06_CONST ; C - high
+	dc.b	#$42, #12, #VOL06_CONST ; A#
 
-        dc.b    #$59, #24, #VOL06_CONST ; F - continue previous
-        dc.b    #$77, #12, #VOL06_CONST ; C - low
-        dc.b    #$42, #24, #VOL06_CONST ; A# - high
-        dc.b    #$3B, #24, #VOL06_CONST ; C - high
-        dc.b    #$42, #12, #VOL06_CONST ; A#
+	; Uses pokey_notes column H (brown)
+	dc.b	#$70, #6,  #VOL06_CONST ; C# (trill)
+	dc.b	#$6A, #90, #VOL06_CONST ; D
 
-        dc.b    #$70, #6,  #VOL06_CONST ; C# (trill)
-        dc.b    #$6A, #90, #VOL06_CONST ; D
+	; Uses pokey_notes column H (brown)
+	dc.b	#$6A, #84, #VOL06_CONST ; D - continue previous
+	dc.b	#$8E, #12,  #VOL06_CONST ; A - low
 
-        dc.b    #$6A, #84, #VOL06_CONST ; D - continue previous
-        dc.b    #$8E, #12,  #VOL06_CONST ; A - low
+	; Uses pokey_notes column H (brown)
+	dc.b	#$86, #84, #VOL06_CONST ; A#
+	dc.b	#$8E, #6,  #VOL06_CONST ; A
+	dc.b	#$86, #6,  #VOL06_CONST ; A#
 
-        dc.b    #$86, #84, #VOL06_CONST ; A#
-        dc.b    #$8E, #6,  #VOL06_CONST ; A
-        dc.b    #$86, #6,  #VOL06_CONST ; A#
-
-        dc.b    #$77, #36, #VOL06_CONST ; C
-        dc.b    #$86, #24, #VOL06_CONST ; A#
-        dc.b    #$77, #12, #VOL06_CONST ; C
-        dc.b    #$6A, #12, #VOL06_CONST ; D
-        dc.b    #$5E, #12, #VOL06_CONST ; E
+	; Uses pokey_notes column H (brown)
+	dc.b	#$77, #36, #VOL06_CONST ; C
+	dc.b	#$86, #24, #VOL06_CONST ; A#
+	dc.b	#$77, #12, #VOL06_CONST ; C
+	dc.b	#$6A, #12, #VOL06_CONST ; D
+	dc.b	#$5E, #12, #VOL06_CONST ; E
 
 	dc.b	#$FF
 	dc.b	#<Goonies2Song1Track2
@@ -109,7 +149,44 @@ Goonies2Song1Track3
 	dc.b	#>Goonies2Song1Track3
   ELSE
 	; Uses pokey_notes column P (green)
-	dc.b	#$2D, #18, #VOL06REST02 ; D
+	dc.b	#$3E, #12, #VOL06_CONST ; A
+	dc.b	#$4F, #12, #VOL06_CONST ; F
+	dc.b	#$6A, #12, #VOL06_CONST ; C
+	dc.b	#$3E, #12, #VOL06_CONST ; A
+	dc.b	#$4F, #12, #VOL06_CONST ; F
+	dc.b	#$6A, #12, #VOL06_CONST ; C
+	dc.b	#$3E, #12, #VOL06_CONST ; A
+	dc.b	#$4F, #12, #VOL06_CONST ; F
+
+	; Uses pokey_notes column P (green)
+	dc.b	#$39, #12, #VOL06_CONST ; A#
+	dc.b	#$4F, #12, #VOL06_CONST ; F
+	dc.b	#$5E, #12, #VOL06_CONST ; D
+	dc.b	#$39, #12, #VOL06_CONST ; A#
+	dc.b	#$4F, #12, #VOL06_CONST ; F
+	dc.b	#$5E, #12, #VOL06_CONST ; D
+	dc.b	#$39, #12, #VOL06_CONST ; A#
+	dc.b	#$4F, #12, #VOL06_CONST ; F
+
+	; Uses pokey_notes column P (green)
+	dc.b	#$33, #12, #VOL06_CONST ; C
+	dc.b	#$46, #12, #VOL06_CONST ; G
+	dc.b	#$53, #12, #VOL06_CONST ; E
+	dc.b	#$33, #12, #VOL06_CONST ; C
+	dc.b	#$46, #12, #VOL06_CONST ; G
+	dc.b	#$53, #12, #VOL06_CONST ; E
+	dc.b	#$33, #12, #VOL06_CONST ; C
+	dc.b	#$46, #12, #VOL06_CONST ; G
+
+	; Uses pokey_notes column P (green)
+	dc.b	#$39, #12, #VOL06_CONST ; A#
+	dc.b	#$4F, #12, #VOL06_CONST ; F
+	dc.b	#$5E, #12, #VOL06_CONST ; D
+	dc.b	#$39, #12, #VOL06_CONST ; A#
+	dc.b	#$4F, #12, #VOL06_CONST ; F
+	dc.b	#$5E, #12, #VOL06_CONST ; D
+	dc.b	#$39, #12, #VOL06_CONST ; A#
+	dc.b	#$4F, #12, #VOL06_CONST ; F
 
 	dc.b	#$FF
 	dc.b	#<Goonies2Song1Track3
@@ -132,16 +209,16 @@ Goonies2Song1Track4
 
 	dc.b	#HIGHSNARE , #4, #DRUM4FRAMES	; bass drum
 	dc.b	#$00, #20, #RESTDEFAULT ; rest
-        dc.b    #SNAREDRUM , #4, #DRUM4FRAMES   ; bass drum
-        dc.b    #$00, #20, #RESTDEFAULT ; rest
-        dc.b    #SNAREDRUM , #4, #DRUM4FRAMES   ; bass drum
-        dc.b    #$00, #20, #RESTDEFAULT ; rest
-        dc.b    #SNAREDRUM , #4, #DRUM4FRAMES   ; bass drum
-        dc.b    #$00, #8, #RESTDEFAULT ; rest
-        dc.b    #HIGHSNARE , #4, #DRUM4FRAMES   ; bass drum
-        dc.b    #$00, #2, #RESTDEFAULT ; rest
-        dc.b    #HIGHSNARE , #4, #DRUM4FRAMES   ; bass drum
-        dc.b    #$00, #2, #RESTDEFAULT ; rest
+	dc.b	#SNAREDRUM , #4, #DRUM4FRAMES	; bass drum
+	dc.b	#$00, #20, #RESTDEFAULT ; rest
+	dc.b	#SNAREDRUM , #4, #DRUM4FRAMES	; bass drum
+	dc.b	#$00, #20, #RESTDEFAULT ; rest
+	dc.b	#SNAREDRUM , #4, #DRUM4FRAMES	; bass drum
+	dc.b	#$00, #8, #RESTDEFAULT ; rest
+	dc.b	#HIGHSNARE , #4, #DRUM4FRAMES	; bass drum
+	dc.b	#$00, #2, #RESTDEFAULT ; rest
+	dc.b	#HIGHSNARE , #4, #DRUM4FRAMES	; bass drum
+	dc.b	#$00, #2, #RESTDEFAULT ; rest
 
 	dc.b	#$FF
 	dc.b	#<Goonies2Song1Track4
