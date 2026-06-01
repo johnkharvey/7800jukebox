0866447ef9a704bb979eb23acb813300e071f578
John K. Harvey <johnkharvey@gmail.com>
2026-06-01 18:17:40 -0400

    RBI music but it sounds sharp

--- src/music_rbibaseball1song1.asm
+++ src/music_rbibaseball1song1.asm
@@ -2,8 +2,8 @@ RBIBASEBALL1SONG1_PLAY_PHRASE1	set	1
 RBIBASEBALL1SONG1_PLAY_PHRASE2	set	1
 
 MUTE_TRACK1		set	0
-MUTE_TRACK2		set	1
-MUTE_TRACK3		set	1
+MUTE_TRACK2		set	0
+MUTE_TRACK3		set	0
 MUTE_TRACK4		set	0
 
 ; Drum equates
@@ -29,7 +29,6 @@ RBIBaseball1Song1Track1
   ELSE
 
   IF RBIBASEBALL1SONG1_PLAY_PHRASE1
-;3,4,4,3?
 	; 84 (7*12)
         ; Uses pokey_notes column P (green)
         dc.b    #MAIN_OCT4_A,  #7,  #VOL06REST02 ; A
@@ -41,7 +40,6 @@ RBIBaseball1Song1Track1
         dc.b    #MAIN_OCT4_A,  #7,  #VOL06REST02 ; A
         dc.b    #RESTNOSOUND,  #35, #RESTDEFAULT ; rest
 
-	; 84 (7*12)
 	; Uses pokey_notes column P (green)
         dc.b    #MAIN_OCT5_A,  #7,  #VOL06REST02 ; A
         dc.b    #MAIN_OCT5_A,  #7,  #VOL06REST02 ; A
@@ -52,7 +50,6 @@ RBIBaseball1Song1Track1
         dc.b    #MAIN_OCT5_A,  #7,  #VOL06REST02 ; A
         dc.b    #RESTNOSOUND,  #35, #RESTDEFAULT ; rest
 
-	; 84
         ; Uses pokey_notes column P (green)
         dc.b    #MAIN_OCT4_A,  #7,  #VOL06REST02 ; A
         dc.b    #MAIN_OCT4_A,  #7,  #VOL06REST02 ; A
@@ -67,7 +64,6 @@ RBIBaseball1Song1Track1
         dc.b    #RESTNOSOUND,  #7,  #RESTDEFAULT ; rest
         dc.b    #MAIN_OCT4_B,  #7,  #VOL06REST02 ; B
 
-	; 84
         ; Uses pokey_notes column P (green)
         dc.b    #MAIN_OCT4_A,  #7,  #VOL06REST02 ; A
         dc.b    #RESTNOSOUND,  #14, #RESTDEFAULT ; rest
@@ -133,13 +129,93 @@ RBIBaseball1Song1Track2
 
   IF RBIBASEBALL1SONG1_PLAY_PHRASE1
 	; Uses pokey_notes column H (brown)
-	dc.b	#$3B, #14, #VOL04_CONST ; C
-	dc.b	#$00, #4,  #RESTDEFAULT ; rest
-	dc.b	#$3B, #9,  #VOL04_CONST ; C
-	dc.b	#$00, #3,  #RESTDEFAULT ; rest
+        dc.b    #RESTNOSOUND,  #63, #RESTDEFAULT ; rest
+        dc.b    #BASS_OCT2_A,  #7,  #VOL06REST02 ; A
+        dc.b    #RESTNOSOUND,  #14, #RESTDEFAULT ; rest
+
+        ; Uses pokey_notes column H (brown)
+        dc.b    #RESTNOSOUND,  #63, #RESTDEFAULT ; rest
+        dc.b    #BASS_OCT2_A,  #7,  #VOL06REST02 ; A
+        dc.b    #RESTNOSOUND,  #14, #RESTDEFAULT ; rest
+
+        ; Uses pokey_notes column H (brown)
+        dc.b    #BASS_OCT2_A,  #7,  #VOL06REST02 ; A
+        dc.b    #BASS_OCT2_A,  #7,  #VOL06REST02 ; A
+        dc.b    #BASS_OCT2_A,  #7,  #VOL06REST02 ; A
+        dc.b    #BASS_OCT2_A,  #7,  #VOL06REST02 ; A
+        dc.b    #RESTNOSOUND,  #7,  #RESTDEFAULT ; rest
+        dc.b    #BASS_OCT2_A,  #7,  #VOL06REST02 ; A
+        dc.b    #BASS_OCT3_A,  #7,  #VOL06REST02 ; A
+        dc.b    #BASS_OCT3_A,  #7,  #VOL06REST02 ; A
+        dc.b    #BASS_OCT3_A,  #7,  #VOL06REST02 ; A
+        dc.b    #BASS_OCT3_A,  #7,  #VOL06REST02 ; A
+        dc.b    #RESTNOSOUND,  #7,  #RESTDEFAULT ; rest
+        dc.b    #BASS_OCT3_A,  #7,  #VOL06REST02 ; A
+
+        ; Uses pokey_notes column H (brown)
+        dc.b    #BASS_OCT2_A,  #7,  #VOL06REST02 ; A
+        dc.b    #RESTNOSOUND,  #14, #RESTDEFAULT ; rest
+        dc.b    #BASS_OCT2_A,  #7,  #VOL06REST02 ; A
+        dc.b    #RESTNOSOUND,  #35, #RESTDEFAULT ; rest
+        dc.b    #BASS_OCT3_A,  #7,  #VOL06REST02 ; A
+        dc.b    #BASS_OCT3_FS, #7,  #VOL06REST02 ; F#
+        dc.b    #BASS_OCT3_E,  #7,  #VOL06REST02 ; E
   ENDIF
 
   IF RBIBASEBALL1SONG1_PLAY_PHRASE2
+        ; Uses pokey_notes column H (brown)
+        dc.b    #BASS_OCT3_D,  #7,  #VOL06REST02 ; D
+        dc.b    #RESTNOSOUND,  #7,  #RESTDEFAULT ; rest
+        dc.b    #BASS_OCT4_D,  #7,  #VOL06REST02 ; D
+        dc.b    #BASS_OCT3_D,  #7,  #VOL06REST02 ; D
+        dc.b    #RESTNOSOUND,  #7,  #RESTDEFAULT ; rest
+        dc.b    #BASS_OCT4_D,  #7,  #VOL06REST02 ; D
+        dc.b    #BASS_OCT3_D,  #7,  #VOL06REST02 ; D
+        dc.b    #RESTNOSOUND,  #7,  #RESTDEFAULT ; rest
+        dc.b    #BASS_OCT4_D,  #7,  #VOL06REST02 ; D
+        dc.b    #BASS_OCT4_CS, #7,  #VOL06REST02 ; C#
+        dc.b    #BASS_OCT3_B,  #7,  #VOL06REST02 ; B
+        dc.b    #BASS_OCT3_A,  #7,  #VOL06REST02 ; A
+
+        ; Uses pokey_notes column H (brown)
+        dc.b    #BASS_OCT3_G,  #7,  #VOL06REST02 ; G
+        dc.b    #RESTNOSOUND,  #7,  #RESTDEFAULT ; rest
+        dc.b    #BASS_OCT4_G,  #7,  #VOL06REST02 ; G
+        dc.b    #BASS_OCT3_G,  #7,  #VOL06REST02 ; G
+        dc.b    #RESTNOSOUND,  #7,  #RESTDEFAULT ; rest
+        dc.b    #BASS_OCT4_G,  #7,  #VOL06REST02 ; G
+        dc.b    #BASS_OCT3_G,  #7,  #VOL06REST02 ; G
+        dc.b    #RESTNOSOUND,  #7,  #RESTDEFAULT ; rest
+        dc.b    #BASS_OCT4_G,  #7,  #VOL06REST02 ; G
+        dc.b    #BASS_OCT4_G,  #7,  #VOL06REST02 ; G
+        dc.b    #BASS_OCT4_D,  #7,  #VOL06REST02 ; D
+        dc.b    #BASS_OCT4_G,  #7,  #VOL06REST02 ; G
+
+        ; Uses pokey_notes column H (brown)
+        dc.b    #BASS_OCT2_A,  #7,  #VOL06REST02 ; A - low
+        dc.b    #RESTNOSOUND,  #7,  #RESTDEFAULT ; rest
+        dc.b    #BASS_OCT3_A,  #7,  #VOL06REST02 ; A
+        dc.b    #BASS_OCT3_G,  #7,  #VOL06REST02 ; G
+        dc.b    #RESTNOSOUND,  #7,  #RESTDEFAULT ; rest
+        dc.b    #BASS_OCT4_G,  #7,  #VOL06REST02 ; G
+        dc.b    #BASS_OCT3_FS, #7,  #VOL06REST02 ; F#
+        dc.b    #RESTNOSOUND,  #7,  #RESTDEFAULT ; rest
+        dc.b    #BASS_OCT4_FS, #7,  #VOL06REST02 ; F# - sounds a little out of tune
+        dc.b    #BASS_OCT3_E,  #7,  #VOL06REST02 ; E
+        dc.b    #RESTNOSOUND,  #7,  #RESTDEFAULT ; rest
+        dc.b    #BASS_OCT4_E,  #7,  #VOL06REST02 ; E
+
+        ; Uses pokey_notes column H (brown)
+        dc.b    #BASS_OCT3_D,  #7,  #VOL06REST02 ; D
+        dc.b    #RESTNOSOUND,  #7,  #RESTDEFAULT ; rest
+        dc.b    #BASS_OCT2_A,  #7,  #VOL06REST02 ; A
+        dc.b    #BASS_OCT2_B,  #7,  #VOL06REST02 ; B
+        dc.b    #BASS_OCT2_B,  #7,  #VOL06REST02 ; B
+        dc.b    #BASS_OCT3_C,  #7,  #VOL06REST02 ; C#
+        dc.b    #BASS_OCT3_D,  #7,  #VOL06REST02 ; D
+        dc.b    #RESTNOSOUND,  #14, #RESTDEFAULT ; rest
+        dc.b    #BASS_OCT3_D,  #7,  #VOL06REST02 ; D
+        dc.b    #RESTNOSOUND,  #14, #RESTDEFAULT ; rest
   ENDIF
 
 RBIBaseball1Song1Track2Stops
@@ -167,9 +243,79 @@ RBIBaseball1Song1Track3
   ELSE
 
   IF RBIBASEBALL1SONG1_PLAY_PHRASE1
+        ; 84 (7*12)
+        ; Uses pokey_notes column P (green)
+        dc.b    #MAIN_OCT3_A,  #7,  #VOL06REST02 ; A
+        dc.b    #MAIN_OCT3_A,  #7,  #VOL06REST02 ; A
+        dc.b    #MAIN_OCT3_A,  #7,  #VOL06REST02 ; A
+        dc.b    #MAIN_OCT3_A,  #7,  #VOL06REST02 ; A
+        dc.b    #RESTNOSOUND,  #7,  #RESTDEFAULT ; rest
+        dc.b    #MAIN_OCT3_G,  #7,  #VOL06REST02 ; A
+        dc.b    #MAIN_OCT3_A,  #7,  #VOL06REST02 ; A
+        dc.b    #RESTNOSOUND,  #35, #RESTDEFAULT ; rest
+
+        ; Uses pokey_notes column P (green)
+        dc.b    #MAIN_OCT3_A,  #7,  #VOL06REST02 ; A
+        dc.b    #MAIN_OCT3_A,  #7,  #VOL06REST02 ; A
+        dc.b    #MAIN_OCT3_A,  #7,  #VOL06REST02 ; A
+        dc.b    #MAIN_OCT3_A,  #7,  #VOL06REST02 ; A
+        dc.b    #RESTNOSOUND,  #7,  #RESTDEFAULT ; rest
+        dc.b    #MAIN_OCT3_G,  #7,  #VOL06REST02 ; A
+        dc.b    #MAIN_OCT3_A,  #7,  #VOL06REST02 ; A
+        dc.b    #RESTNOSOUND,  #35, #RESTDEFAULT ; rest
+
+        ; Uses pokey_notes column P (green)
+        dc.b    #MAIN_OCT4_CS, #7,  #VOL06REST02 ; C#
+        dc.b    #MAIN_OCT4_CS, #7,  #VOL06REST02 ; C#
+        dc.b    #MAIN_OCT4_CS, #7,  #VOL06REST02 ; C#
+        dc.b    #MAIN_OCT4_CS, #7,  #VOL06REST02 ; C#
+        dc.b    #RESTNOSOUND,  #7,  #RESTDEFAULT ; rest
+        dc.b    #MAIN_OCT3_B,  #7,  #VOL06REST02 ; B
+        dc.b    #MAIN_OCT3_A,  #7,  #VOL06REST02 ; A
+        dc.b    #RESTNOSOUND,  #7,  #RESTDEFAULT ; rest
+        dc.b    #MAIN_OCT3_B,  #7,  #VOL06REST02 ; B
+        dc.b    #MAIN_OCT4_CS, #7,  #VOL06REST02 ; C#
+        dc.b    #RESTNOSOUND,  #7,  #RESTDEFAULT ; rest
+        dc.b    #MAIN_OCT4_D,  #7,  #VOL06REST02 ; D
+
+        ; Uses pokey_notes column P (green)
+        dc.b    #MAIN_OCT4_CS, #7,  #VOL06REST02 ; C#
+        dc.b    #RESTNOSOUND,  #14, #RESTDEFAULT ; rest
+        dc.b    #MAIN_OCT4_CS, #7,  #VOL06REST02 ; C#
+        dc.b    #RESTNOSOUND,  #35, #RESTDEFAULT ; rest
+        dc.b    #MAIN_OCT4_A,  #7,  #VOL06REST02 ; A
+        dc.b    #MAIN_OCT4_A,  #7,  #VOL06REST02 ; A
+        dc.b    #MAIN_OCT4_A,  #7,  #VOL06REST02 ; A
+
   ENDIF
 
   IF RBIBASEBALL1SONG1_PLAY_PHRASE2
+        ; Uses pokey_notes column P (green)
+        dc.b    #MAIN_OCT4_FS, #21,  #VOL06REST02 ; F#
+        dc.b    #MAIN_OCT4_FS, #21,  #VOL06REST02 ; F#
+        dc.b    #MAIN_OCT3_A,  #21,  #VOL06REST02 ; A
+        dc.b    #MAIN_OCT3_A,  #21,  #VOL06REST02 ; A
+
+        ; Uses pokey_notes column P (green)
+        dc.b    #MAIN_OCT4_D,  #21, #VOL06REST02 ; D
+        dc.b    #MAIN_OCT4_G,  #7,  #VOL06REST02 ; G
+        dc.b    #MAIN_OCT4_B,  #7,  #VOL06REST02 ; B
+        dc.b    #MAIN_OCT5_D,  #7,  #VOL06REST02 ; D
+        dc.b    #MAIN_OCT5_G,  #7,  #VOL06REST02 ; G
+        dc.b    #RESTNOSOUND,  #14, #RESTDEFAULT ; rest
+        dc.b    #MAIN_OCT4_D,  #7,  #VOL06REST02 ; D
+        dc.b    #MAIN_OCT4_E,  #7,  #VOL06REST02 ; E
+        dc.b    #MAIN_OCT4_D,  #7,  #VOL06REST02 ; D
+
+        ; Uses pokey_notes column P (green)
+        dc.b    #MAIN_OCT4_A,  #21, #VOL06REST02 ; A
+        dc.b    #MAIN_OCT4_A,  #21, #VOL06REST02 ; A
+        dc.b    #MAIN_OCT4_A,  #21, #VOL06REST02 ; A
+        dc.b    #MAIN_OCT4_A,  #21, #VOL06REST02 ; A
+
+        ; Uses pokey_notes column P (green)
+        dc.b    #MAIN_OCT4_FS, #56, #VOL06REST02 ; F#
+        dc.b    #RESTNOSOUND,  #28, #RESTDEFAULT ; rest
   ENDIF
 
 RBIBaseball1Song1Track3Stops
